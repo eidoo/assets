a95522d53a041bdb9b041a0702502b6c4c296707
Alessandro Manfredi <alle.manfredi@gmail.com>
2021-10-07 10:42:17 +0200

    refactor(blockchains): starts rm checksum

---
+++ script/remove-checksum.js
@@ -0,0 +1,11 @@
+const { readdirSync, renameSync } = require('fs')
+
+const removeChecksum = (_source, _temp) =>
+  readdirSync(_source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .forEach(dirent => {
+      renameSync(`${_source}/${dirent.name}`, `${_temp}/${dirent.name.toLowerCase()}`)
+    })
+
+
+removeChecksum(process.argv[2], process.argv[3])
