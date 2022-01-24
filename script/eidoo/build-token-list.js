c953a20455a023fa8a4c7256290f6eafad3e681e
Massimiliano D'Elia <mdelia@eidoo.io>
2022-01-24 14:30:25 +0100

    feat(scripts): updates build-token-list to output only the first 200 cryptocurrencies in CMC

--- script/eidoo/build-token-list.js
+++ script/eidoo/build-token-list.js
@@ -1,21 +1,39 @@
-const { readdirSync, readFileSync, writeFileSync } = require('fs')
+const axios = require('axios')
 const beautify = require('json-beautify')
+const { readdirSync, readFileSync, writeFileSync } = require('fs')
 
-const dir = `${process.argv[2]}/assets`
+const overrides = require('./build-token-list_overrides.json')
 
-const tokens = []
-readdirSync(dir, { withFileTypes: true })
-  .filter(_dirent => _dirent.isDirectory())
-  .forEach(_dirent => {
-    const { name, type, decimals, symbol, description}  = JSON.parse(readFileSync(`${dir}/${_dirent.name}/info.json`, 'utf-8'))
-    tokens.push({
-      name,
-      type,
-      decimals,
-      symbol,
-      description,
-      address: _dirent.name.toLowerCase()
-    })
+;(async () => {
+  const { data: resBody } = await axios({
+    method: 'get',
+    url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=200&sort=cmc_rank',
+    headers: {
+      'X-CMC_PRO_API_KEY': 'REPLACEME'
+    }
   })
 
-writeFileSync(`${process.argv[2]}/tokenlist_v2.json`, beautify(tokens, null, 2, 100))
+  const overrideSymbols = overrides.symbols.map((x) => x.toLowerCase())
+  const topSymbols = resBody.data.map((x) => x.symbol.toLowerCase())
+
+  const dir = `${process.argv[2]}/assets`
+  const tokens = []
+  readdirSync(dir, { withFileTypes: true })
+    .filter(_dirent => _dirent.isDirectory())
+    .forEach(_dirent => {
+      const file = readFileSync(`${dir}/${_dirent.name}/info.json`, 'utf-8')
+      const { name, type, decimals, symbol, description}  = JSON.parse(file)
+      if (overrideSymbols.includes(symbol.toLowerCase()) || topSymbols.includes(symbol.toLowerCase())) {
+        tokens.push({
+          name,
+          type,
+          decimals,
+          symbol,
+          description,
+          address: _dirent.name.toLowerCase()
+        })
+      }
+    })
+
+  writeFileSync(`${process.argv[2]}/tokenlist_v2.json`, beautify(tokens, null, 2, 100))
+})()
