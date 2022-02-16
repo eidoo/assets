5c5b62f9f56c542cec6ab8ab54ca4f5318d622ef
Massimiliano D'Elia <mdelia@eidoo.io>
2022-02-16 12:10:11 +0100

    feat(scripts): adds replace assets type script

--- script/eidoo/build.sh
+++ script/eidoo/build.sh
@@ -8,6 +8,14 @@ node script/eidoo/remove-checksum.js blockchains/fantom_mainnet
 node script/eidoo/remove-checksum.js blockchains/polygon_mainnet
 # Please notice: No Solana here! Its assets need to keep the checksum.
 
+echo "Replace assets type..."
+node script/eidoo/replace-assets-type.js blockchains/avalanchec_mainnet
+node script/eidoo/replace-assets-type.js blockchains/bsc_mainnet
+node script/eidoo/replace-assets-type.js blockchains/eth_mainnet
+node script/eidoo/replace-assets-type.js blockchains/fantom_mainnet
+node script/eidoo/replace-assets-type.js blockchains/polygon_mainnet
+node script/eidoo/replace-assets-type.js blockchains/solana_mainnet
+
 echo "Building token lists..."
 node script/eidoo/build-token-list.js blockchains/avalanchec_mainnet
 node script/eidoo/build-token-list.js blockchains/bsc_mainnet
