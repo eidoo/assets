291aedd321aa533c0af619b0f1c0fc6c0bf67742
Massimiliano D'Elia <mdelia@eidoo.io>
2022-02-28 10:49:32 +0100

    fix(retailers): fixes some logos

--- script/eidoo/retailers.js
+++ script/eidoo/retailers.js
@@ -8,6 +8,30 @@ const newOutput = {}
 const output = {}
 const supportedExtensions = ["png"]
 
+const addURLEncode = (fileName) => {
+  return fileName
+    .replace(new RegExp("&", "g"), "%26")
+    .replace(new RegExp("à", "g"), "%C3%A0")
+    .replace(new RegExp("ã", "g"), "%C3%A3")
+    .replace(new RegExp("ä", "g"), "%C3%A4")
+    .replace(new RegExp("å", "g"), "%C3%A5")
+    .replace(new RegExp("ç", "g"), "%C3%A7")
+    .replace(new RegExp("è", "g"), "%C3%A8")
+    .replace(new RegExp("é", "g"), "%C3%A9")
+    .replace(new RegExp("ê", "g"), "%C3%AA")
+    .replace(new RegExp("ë", "g"), "%C3%AB")
+    .replace(new RegExp("ì", "g"), "%C3%AC")
+    .replace(new RegExp("î", "g"), "%C3%AE")
+    .replace(new RegExp("ï", "g"), "%C3%AF")
+    .replace(new RegExp("ò", "g"), "%C3%B2")
+    .replace(new RegExp("ô", "g"), "%C3%B4")
+    .replace(new RegExp("ö", "g"), "%C3%B6")
+    .replace(new RegExp("ù", "g"), "%C3%B9")
+    .replace(new RegExp("û", "g"), "%C3%BB")
+    .replace(new RegExp("ü", "g"), "%C3%BC")
+    .replace(new RegExp("\\+", "g"), "%2B")
+}
+
 readdirSync(dir, { withFileTypes: true })
   .forEach(_dirent => {
     const parts = _dirent.name.split('.')
@@ -21,15 +45,13 @@ readdirSync(dir, { withFileTypes: true })
     const fileOverrides = overrides[fileName]
     if (fileOverrides) {
       if (Array.isArray(fileOverrides)) {
-        fileOverrides.forEach(fileOverride => { output[fileOverride] =  fileName })
+        fileOverrides.forEach(fileOverride => { output[fileOverride] =  addURLEncode(fileName) })
       } else {
-        output[fileOverrides] =  fileName
+        output[fileOverrides] =  addURLEncode(fileName)
       }
     } else {
       const key = fileName.replace(new RegExp("_", "g"), " ")
-      output[key] = fileName
-        .replace(new RegExp("&", "g"), "%26")
-        .replace(new RegExp("\\+", "g"), "%2B")
+      output[key] = addURLEncode(fileName)
     }
 
     for (const key of Object.keys(output)) {
